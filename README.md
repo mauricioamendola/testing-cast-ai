16731e8d16d285c2fe73edc7a5962dceb8c1af6a
Mauricio <mauricio@linux.com.uy>
2024-07-23 09:43:51 -0300

    Added tf code to deploy a cluster for Cast.ai

---
+++ README.md
@@ -0,0 +1,27 @@
+# Playing around with [Cast.ai](https://cast.ai/)
+
+## Playground deployment
+
+```sh {"id":"01J3D9GR1M0SZR0ECB8Q79Y7V7","promptEnv":"yes"}
+cd tf-cluster
+export TF_VAR_region="us-east-1"
+export AWS_DEFAULT_REGION="us-east-1"
+export AWS_ACCESS_KEY_ID=[ACCESS_KEY]
+export AWS_SECRET_ACCESS_KEY=[Secret KEY]
+```
+
+```sh {"id":"01J3D9H86AVGCC6P3TN4KFF5SH"}
+cd tf-cluster
+terraform init
+terraform plan
+
+```
+
+```sh {"id":"01J3D9HDYJGRC2CPJR964FJ88C"}
+cd tf-cluster && terraform apply
+```
+
+```sh {"id":"01J3DRX1607DY3GW6TK2XB1BX9"}
+## Clean resources
+terraform destroy
+```
---
+++ tf-cluster/main.tf
@@ -0,0 +1,95 @@
+provider "aws" {
+  region = var.region
+}
+
+# Filter out local zones, which are not currently supported 
+# with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+locals {
+  cluster_name = "cast-poc-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 4
+  special = false
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "cast-poc-eks-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway      = false
+  single_nat_gateway      = false
+  enable_dns_hostnames    = true
+  enable_dns_support      = true
+  map_public_ip_on_launch = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+}
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.29"
+
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+  create_cluster_security_group = true
+  create_node_security_group    = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["t3.micro"]
+  }
+
+  eks_managed_node_groups = {
+    oss-ng01 = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 3
+
+      instance_types = ["t3.micro"]
+      capacity_type  = "ON_DEMAND"
+    }
+  }
+
+
+  enable_cluster_creator_admin_permissions = true
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
